1a63dcbdf9df000a7f5a1b5f1be3f581d4f20d7a
iBinh <ntbinh21@gmail.com>
2021-11-18 06:20:23 +0000

    version bump

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,16 @@
-use std::ops::Deref;
-
 use yada::{DoubleArray, builder::DoubleArrayBuilder};
 
-pub struct Map<K, V>
-    where K: Deref<Target = [u8]> + std::convert::AsRef<[u8]>
+pub struct Map<V>
 {
     pub trie: DoubleArray<Vec<u8>>,
     pub data: Vec<V>,
-    phantom: std::marker::PhantomData<K>
 }
 
-impl<K, V> Map<K, V> 
-    where K: Deref<Target = [u8]> + std::convert::AsRef<[u8]>
+impl<V> Map<V> 
 {
     /// Creates a new map from a iterator of key, value pairs that has to be presorted
-    pub fn build(pairs: impl Iterator<Item = (K, V)>, size_hint: usize) -> Option<Self> {
-        let mut keys: Vec<(K, u32)> = Vec::with_capacity(size_hint);
+    pub fn build(pairs: impl Iterator<Item = (String, V)>, size_hint: usize) -> Option<Self> {
+        let mut keys: Vec<(String, u32)> = Vec::with_capacity(size_hint);
         let mut data: Vec<V> = Vec::with_capacity(size_hint);
         for (index, (key, value)) in pairs.enumerate() {
             keys.push((key, index as u32));
@@ -24,12 +19,11 @@ impl<K, V> Map<K, V>
         Some(Map {
             trie: DoubleArray::new(DoubleArrayBuilder::build(&keys)?),
             data,
-            phantom: std::marker::PhantomData
         })
     }
     /// Returns the value associated with the key
     /// Returns None if the key is not found
-    pub fn get(&self, key: &K) -> Option<&V> {
-        self.trie.exact_match_search(&key).map(|index| &self.data[index as usize])
+    pub fn get(&self, key: &str) -> Option<&V> {
+        self.trie.exact_match_search(key.as_bytes()).map(|index| &self.data[index as usize])
     }
 }
